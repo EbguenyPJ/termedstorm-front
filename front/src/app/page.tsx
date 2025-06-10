3bf7ec8ce2c7542923e6430717f02f59c94c98a4
IWeppler <ignacionweppler@gmail.com>
2025-06-09 21:04:33 -0300

    first commit

---
+++ front/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-5xl">Aca entonces iria un register/login?</h1>
+    </div>
+  );
+}
---
+++ front/src/app/routes/index.ts
@@ -0,0 +1,24 @@
+export const routes = {
+  //pueden ver todos (segun el rol puede cambiar)
+  home: "/",
+  login: "/login",
+  register: "/register",
+  categories: "/categories",
+  subcategories: "/subcategories",
+  products: "/products",
+  //cart: '/cart', (NO es necesario si trabajamos con modal)
+  profile: "/profile", //informacion personal basica
+  reports: "/reports", //cambia con la autorizacion respectiva
+  support: "/support", // cambia con la autorización respectiva
+  // solo gerente
+  addProduct: "addproduct",
+  addCategory: "addcategory",
+  addSubCategory: "addsubcategory",
+  addBrand: "addbrand",
+  editLists: "editLists",
+  // contador o caja
+  cashCount: "cashcount", //historial de arqueos
+  newCashCount: "cashcount", //registrar nuevo arqueo
+  newCashRegister: "cashregister", //registrar nuevo corte
+  orders: "orders", //lista de venta/ordenes
+};
