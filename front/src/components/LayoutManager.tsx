4b4ec478738b073ee0f129200d94128a9e82dda7
EmilianoAllende <emilianoallende94@gmail.com>
2025-06-11 01:13:21 -0300

    feat: navbar implemented
    There are details yet to optimize, but it's functional and responsive.

--- front/src/components/LayoutManager.tsx
+++ front/src/components/LayoutManager.tsx
@@ -1,3 +1,78 @@
+// "use client";
+
+// import { useRef, useState, useEffect } from "react";
+// import Navbar from "@/components/Navbar/Navbar";
+// import SideBar from "@/components/SideBar";
+// import ExcludedWrapper from "@/components/ExcludedWrapper";
+// import { useOutsideClick } from "@/hooks/useOutsideClick";
+
+// export default function LayoutManager({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     const [isSidebarCollapsed, setSidebarCollapsed] = useState(true);
+//     const [isMobileOverlay, setIsMobileOverlay] = useState(false);
+
+//     const sidebarRef = useRef<HTMLDivElement>(null);
+
+//     const toggleSidebarCollapse = () => {
+//         setSidebarCollapsed(!isSidebarCollapsed);
+//     };
+
+//     useEffect(() => {
+//         const handleResize = () => {
+//         if (window.innerWidth < 1024) {
+//             setSidebarCollapsed(true);
+//         }
+//         };
+//         handleResize();
+//         window.addEventListener("resize", handleResize);
+//         return () => window.removeEventListener("resize", handleResize);
+//     }, []);
+
+//     useEffect(() => {
+//         if (typeof window !== "undefined") {
+//         const isMobile = window.innerWidth < 1024;
+//         setIsMobileOverlay(!isSidebarCollapsed && isMobile);
+//         }
+//     }, [isSidebarCollapsed]);
+
+//     useOutsideClick(sidebarRef, () => {
+//         if (window.innerWidth < 1024 && !isSidebarCollapsed) {
+//         setSidebarCollapsed(true);
+//         }
+//     });
+
+//     return (
+//         <>
+//         {isMobileOverlay && (
+//             <div className="fixed inset-0 bg-black bg-opacity-50 z-30"></div>
+//         )}
+
+//         <ExcludedWrapper>
+//             <SideBar
+//             ref={sidebarRef}
+//             isCollapsed={isSidebarCollapsed}
+//             toggleCollapse={toggleSidebarCollapse}
+//             />
+//         </ExcludedWrapper>
+
+//         <div
+//             className={`transition-none duration-0 ${
+//             isSidebarCollapsed ? "sm:ml-20" : "sm:ml-64"
+//             }`}
+//         >
+//             <ExcludedWrapper>
+//             <Navbar />
+//             </ExcludedWrapper>
+//             <div className="p-4 absolute">{children}</div>
+//         </div>
+//         </>
+//     );
+// };
+
+
 "use client";
 
 import { useRef, useState, useEffect } from "react";
@@ -13,8 +88,10 @@ export default function LayoutManager({
 }) {
     const [isSidebarCollapsed, setSidebarCollapsed] = useState(true);
     const [isMobileOverlay, setIsMobileOverlay] = useState(false);
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
     const sidebarRef = useRef<HTMLDivElement>(null);
+    const navbarRef = useRef<HTMLDivElement>(null);
 
     const toggleSidebarCollapse = () => {
         setSidebarCollapsed(!isSidebarCollapsed);
@@ -24,6 +101,7 @@ export default function LayoutManager({
         const handleResize = () => {
         if (window.innerWidth < 1024) {
             setSidebarCollapsed(true);
+            setIsNavbarOpen(false);
         }
         };
         handleResize();
@@ -41,6 +119,12 @@ export default function LayoutManager({
     useOutsideClick(sidebarRef, () => {
         if (window.innerWidth < 1024 && !isSidebarCollapsed) {
         setSidebarCollapsed(true);
+        }
+    });
+
+    useOutsideClick(navbarRef, () => {
+        if (window.innerWidth < 1024 && isNavbarOpen) {
+        setIsNavbarOpen(false);
         }
     });
 
@@ -64,8 +148,14 @@ export default function LayoutManager({
             }`}
         >
             <ExcludedWrapper>
-            <Navbar />
+            <div ref={navbarRef}>
+                <Navbar
+                isOpen={isNavbarOpen}
+                toggleMenu={() => setIsNavbarOpen(!isNavbarOpen)}
+                />
+            </div>
             </ExcludedWrapper>
+
             <div className="p-4 absolute">{children}</div>
         </div>
         </>
--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,46 +1,97 @@
+// "use client";
+
+// import { useState } from "react";
+// import Cart from "../Cart";
+// import SearchBar from "../SearchBar";
+// import { UserWidget } from "./UserWidget/UserWidget";
+// import menuOpen from '@/utils/menu-navigation-open.svg';
+// import menuClose from '@/utils/menu-navigation-closed.svg';
+// import Image from "next/image";
+
+// export default function Navbar() {
+//     const [isOpen, setIsOpen] = useState(false);
+
+//     const toggleMenu = () => setIsOpen(!isOpen);
+
+//     return (
+//         <nav className="relative bg-secondary text-base w-full md:w-10/12 md:fixed md:right-0 md:flex md:items-center md:justify-around md:h-16 md:px-2 md:rounded-none text-base1 rounded-lg">
+//             <div className="absolute p-2 rounded-lg mt-2 right-2 bg-secondary md:hidden">
+//                 <button onClick={toggleMenu} className="text-2xl m-auto">
+//                     {isOpen ? (
+//                         <Image src={menuOpen} alt="menuOpen" className="w-8" />
+//                     ) : (
+//                         <Image src={menuClose} alt="menuClose" className="w-8" />
+//                     )}
+//                 </button>
+//             </div>
+
+//             <div className={`${isOpen ? "flex" : "hidden"} flex-col-reverse gap-4 md:flex md:flex-row md:items-center md:justify-between md:mt-0 md:gap-0 w-full md:w-auto`}>
+                
+//                     <SearchBar 
+//                     />
+
+//                 <div className="flex flex-row-reverse w-1/2 justify-between mt-2 ml-2 md:flex-row">
+//                     <div className="my-auto">
+//                         <Cart />
+//                     </div>
+
+//                     <div className="flex my-auto md:ml-auto">
+//                         <UserWidget />
+//                     </div>
+//                 </div>
+
+//             </div>
+//         </nav>
+//     );
+// };
+
+
 "use client";
 
-import { useState } from "react";
 import Cart from "../Cart";
 import SearchBar from "../SearchBar";
 import { UserWidget } from "./UserWidget/UserWidget";
-import menuOpen from '@/utils/menu-navigation-open.svg';
-import menuClose from '@/utils/menu-navigation-closed.svg';
+import menuOpen from "@/utils/menu-navigation-open.svg";
+import menuClose from "@/utils/menu-navigation-closed.svg";
 import Image from "next/image";
 
-export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
+type NavbarProps = {
+    isOpen: boolean;
+    toggleMenu: () => void;
+};
 
-    const toggleMenu = () => setIsOpen(!isOpen);
-
+export default function Navbar({ isOpen, toggleMenu }: NavbarProps) {
     return (
-        <nav className="relative bg-secondary text-base w-full md:w-10/12 md:fixed md:right-0 md:flex md:items-center md:justify-around md:h-16 md:px-2 md:rounded-none text-base1 rounded-lg">
-            <div className="absolute p-2 rounded-lg mt-2 right-2 bg-secondary md:hidden">
-                <button onClick={toggleMenu} className="text-2xl m-auto">
-                    {isOpen ? (
-                        <Image src={menuOpen} alt="menuOpen" className="w-8" />
-                    ) : (
-                        <Image src={menuClose} alt="menuClose" className="w-8" />
-                    )}
-                </button>
-            </div>
+        <>
+        <div className="fixed top-2 right-2 z-50 md:hidden">
+            <button onClick={toggleMenu} className="bg-primary p-2 rounded-lg">
+            {isOpen ? (
+                <Image src={menuOpen} alt="menuOpen" className="w-8" />
+            ) : (
+                <Image src={menuClose} alt="menuClose" className="w-8" />
+            )}
+            </button>
+        </div>
 
-            <div className={`${isOpen ? "flex" : "hidden"} flex-col-reverse gap-4 md:flex md:flex-row md:items-center md:justify-between md:mt-0 md:gap-0 w-full md:w-auto`}>
-                
-                    <SearchBar 
-                    />
+        <nav
+            className={`fixed top-0 right-0 h-full w-3/4 sm:w-2/4 md:static md:h-16 md:w-full md:flex md:items-center md:justify-around md:rounded-none z-40 bg-primary text-base1 shadow-md transition-transform duration-300 ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+            } md:translate-x-0`}
+        >
+            <div className="flex flex-col-reverse gap-4 md:flex md:flex-row md:items-center md:justify-between md:mt-0 md:gap-0 w-full md:w-auto p-4 md:p-0">
+            <SearchBar />
 
-                <div className="flex flex-row-reverse w-1/2 justify-between mt-2 ml-2 md:flex-row">
-                    <div className="my-auto">
-                        <Cart />
-                    </div>
-
-                    <div className="flex my-auto md:ml-auto">
-                        <UserWidget />
-                    </div>
+            <div className="flex flex-row-reverse w-full md:w-auto justify-between mt-2 md:mt-0 md:ml-2">
+                <div className="my-auto">
+                <Cart />
                 </div>
 
+                <div className="flex my-auto md:ml-auto">
+                <UserWidget />
+                </div>
+            </div>
             </div>
         </nav>
+        </>
     );
 };
