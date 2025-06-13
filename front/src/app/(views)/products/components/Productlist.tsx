bca98812c6dd344dd411e9577cab88f5c05fdb20
karenmmedina <kameme115@gmail.com>
2025-06-13 15:58:49 -0300

    llamados al back en products, categories, subcategories

---
+++ front/src/app/(views)/products/components/Productlist.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ICardProduct } from "@/interfaces";
+import CardProduct from "@/components/UI/Cards/CardProduct";
+
+const ProductList = () => {
+    const [products, setProducts] = useState<ICardProduct[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+            .then((res) => setProducts(res.data))
+            .catch((err) => console.error("Error al obtener productos:", err))
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <p>Cargando productos...</p>;
+
+    return (
+        <div className="flex flex-wrap justify-center gap-y-10">
+            {products.length > 0 ? (
+                products.map((product, index) => (
+                    <CardProduct
+                        key={index}
+                        name={product.name}
+                        image={product.image}
+                        price={product.price}
+                    />
+                ))
+            ) : (
+                <div className="w-full h-60 flex justify-center items-center">
+                    <p className="text-center text-gray-500 text-lg w-full">
+                        No hay productos disponibles.
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ProductList;
