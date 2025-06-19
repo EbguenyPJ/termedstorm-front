8a3e52e4a355841b44578364ae1aab30489b8d49
IWeppler <ignacionweppler@gmail.com>
2025-06-19 15:42:41 -0300

    upload change button google success

--- front/src/app/(views)/auth/success/page.tsx
+++ front/src/app/(views)/auth/success/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import GoogleSuccessClient from "./GoogleSuccess";
+import GoogleSuccess from "./GoogleSuccess";
 
 export default function GoogleSuccessPage() {
   return (
     <Suspense fallback={<p className="text-center mt-10">Cargando...</p>}>
-      <GoogleSuccessClient />
+      <GoogleSuccess />
     </Suspense>
   );
 }
